b7c7be369764000333f15dee6ce2f434acba4ac4
Simon Olofsson <telsak@gmail.com>
2025-09-09 15:11:39 +0200

    basic student picker

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ class Res:
     course_date: str = ''
     all_courses: list = field(default_factory=list)
     favorite_courses: list = field(default_factory=list)
+    student_id: str = ''
+    student_name: str = 'Ingen vald'
+    student_email: str = ''
+    all_students: list = field(default_factory=list)
     w_base_url: str = 'https://hv.instructure.com/api/v1'
     w_token: str = init_auth()
     w_header = {"Authorization":f"Bearer {w_token}"}
@@ -42,6 +46,7 @@ class Menu:
             'course_main_menu': self.course_main_menu,
             'pick_course': self.pick_course_menu,
             'student_main_menu': self.student_main_menu,
+            'pick_student': self.pick_student_menu,
             'submit_course_id': submit_course_id
         }
         
@@ -54,10 +59,11 @@ class Menu:
     def render_status(self, res):
         #os.system(res.term_clear)
         name_len = len(res.course_name)
-        cols = name_len + 8
+        name_len += len(res.student_name)
+        cols = name_len + 20
 
         print(f'\n╔{"═" * cols}╗')
-        print(f'║ Kurs: {res.course_name} ║')
+        print(f'║ Kurs: {res.course_name} ║ Student: {res.student_name} ║')
 
     def main_menu(self, res):
         options = ['Courses']
@@ -118,8 +124,28 @@ class Menu:
             choices=courses,
             default=courses[0],
         ).execute()
-        
+    
         res.course_id, res.course_name, res.course_date = course.split(':')
+        self.state = 'main'
+
+    def pick_student_menu(self, res):
+        users = get_student_list(res) or []
+        if not users:
+            self.state = 'main'
+            return
+
+        choice = inquirer.select(
+            message='Student: ', choices=users + ['Back', 'Quit'],
+        ).execute()
+
+        if choice == 'Back':
+            self.state = 'main'
+            return
+        elif choice == 'Quit':
+            raise SystemExit
+
+        res.student_name, res.student_id, res.student_email = choice.split(',')
+        print(choice)
         self.state = 'main'
         
     def student_main_menu(self, res):
@@ -159,6 +185,25 @@ def get_course_list(category, res):
     # sorts them so they show up in alphabetical order. Array is [courseid:name:date]
     courses.sort(key=lambda c: c.split(':')[1].strip())
     return courses
+
+def get_student_list(res, user_id='aaaa0000'):
+    url = f'{res.w_base_url}/courses/{res.course_id}/users?enrollment_type=student&per_page=100'
+    response = requests.get(url, headers=res.w_header)
+    data = response.json()
+
+    while response.links.get('next') is not None:
+        url = response.links['next']['url']
+        response = requests.get(url, headers=res.w_header)
+        data.extend(response.json())
+    data.sort(key=lambda u: u['sortable_name'].split(',')[0].strip())
+    
+    users = []
+    for user in data:
+        last, first = user["sortable_name"].split(',')
+        print(f'{first} {last},{user["login_id"]},{user["email"]}')
+        users.append(f'{first} {last},{user["login_id"]},{user["email"]}')
+    
+    return users
 
 def submit_course_id(res):
     verified_course_code = False
